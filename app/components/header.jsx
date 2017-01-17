0bb6cc1c7bf25759f45939251b0f0b54b8371021
Rodolfo Palacios <rpalacios@igloosoftware.com>
2017-01-17 16:23:38 -0500

    Header hover.

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -10,9 +10,7 @@ const styles = {
     margin: 'auto'
   },
   title: {
-    fontSize: '2em',
-    fontWeight: '900',
-    padding: '10px 0'
+    
   },
   logo: {
     marginLeft: '15px',
@@ -21,11 +19,42 @@ const styles = {
 };
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: false
+    };
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
+  }
+
+  titleStyle() {
+    let titleStyle = {
+      fontSize: '2em',
+      fontWeight: '900',
+      padding: '10px 0'
+    };
+
+    if (this.state && this.state.hovered) {
+      titleStyle.color = 'white';
+    }
+
+    return titleStyle;
+  }
+
+  onMouseOver() {
+    this.setState({ hovered:true });
+  }
+
+  onMouseOut() {
+    this.setState({ hovered:false });
+  }
+
   render() {
     return (
       <header>
         <div style={styles.header}>
-          <Link to="/" style={styles.title}>COOKBOOK<FontAwesome style={styles.logo} name="cutlery"/></Link>
+          <Link to="/" style={this.titleStyle()} onMouseOut={this.onMouseOut} onMouseOver={this.onMouseOver}>COOKBOOK<FontAwesome style={styles.logo} name="cutlery"/></Link>
         </div>
       </header>
     );
